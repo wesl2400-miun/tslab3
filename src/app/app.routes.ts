196864101436ea481cd0f0a00349914b1daa46b8
= <=>
2026-05-10 14:20:06 +0200

    felsök: Fixa NG04002-felet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { Picture } from './ui/component/picture/picture';
 
 export const routes: Routes = [
   { path: '', component: About },
-  { path: 'about', component: About },
-  { path: 'convert', component: Convert },
-  { path: 'picture', component: Picture }
+  { path: 'about', component: About, pathMatch: 'full' },
+  { path: 'convert', component: Convert, pathMatch: 'full'  },
+  { path: 'picture', component: Picture, pathMatch: 'full'  }
 ];
