6c9d53f03218cdf010c2c84134ceef8fb9296155
= <=>
2026-05-11 15:40:43 +0200

    konfig: Konfigurera rutter

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,9 @@ import { Convert } from './ui/page/convert/convert';
 import { Picture } from './ui/page/picture/picture';
 
 // Här definieras rutterna för hela applikationen
-// Matcha rutterna efter fullständiga sökvägar
 export const routes: Routes = [
-  { path: '', redirectTo: 'about', pathMatch: 'full'},
-  { path: 'about', component: About, pathMatch: 'full' },
-  { path: 'convert', component: Convert, pathMatch: 'full'  },
-  { path: 'picture', component: Picture, pathMatch: 'full'  }
+  { path: '', redirectTo: 'about' },
+  { path: 'about', component: About },
+  { path: 'convert', component: Convert },
+  { path: 'picture', component: Picture  }
 ];
