25dc9511a4c1a35b7ed9278aba763ebf17e7953d
= <=>
2026-05-12 12:16:15 +0200

    ui: Lägg till bilder

---
+++ src/app/logic/model/Image.ts
@@ -0,0 +1,11 @@
+
+export class Image {
+  public src: string;
+  public alt: string;
+
+  constructor(src: string, 
+    alt: string) {
+    this.src = src;
+    this.alt = alt;
+  }
+}
--- src/app/ui/page/picture/picture.ts
+++ src/app/ui/page/picture/picture.ts
@@ -1,9 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Image } from '../../../logic/model/Image';
 
 @Component({
   selector: 'app-picture',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './picture.html',
   styleUrl: './picture.css',
 })
-export class Picture {}
+export class Picture {
+  public images: Image[];
+
+  constructor() {
+    this.images = [
+      new Image('coast1', 
+        'Havsvågor slår mot en klippa'),
+      new Image('coast2', 
+        'En stenbro går över ' + 
+        'ett hav och leder till en fyr'),
+      new Image('coast3', 
+        'Havsvågor slåt mot en klippig strand'),
+    ];
+  }
+}
